d774447dd9cf87b9043e0158e158c145ce93f5f1
Joacohazee <joacohaze@gmail.com>
2022-03-28 11:28:48 -0300

    rutas updateUser y getUser

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const userRouter = express.Router()
-const { Register, Login, GetAllUsers, DeleteUser, Me}= require('../controllers/userControllers')
+const { Register, Login, GetAllUsers, DeleteUser, Me, UpdateUser, GetUser}= require('../controllers/userControllers')
 const verifyToken = require("./validate-token");
 
 
@@ -8,13 +8,14 @@ userRouter.post('/register', Register) //Registro de usuario
 
 userRouter.post('/login', Login) //logueo de usuario
 
-userRouter.get('/me', verifyToken, Me)
+userRouter.get('/me', verifyToken,  Me)
 
 userRouter.get('/allUsers', GetAllUsers) // traer todos los usuarios
+
+userRouter.get('/:id', GetUser) // traer un usuario
+
+userRouter.put('/updateUser/:id', UpdateUser) // editar un usuario
 
 userRouter.delete('/deleteUser/:id', DeleteUser) // elimina un usuario
 
 module.exports = userRouter
-
-
-
