a2d0cd80bcfcf7af57e6dc2c60895cf1415eba9a
Ivan Winslow <ivan_winslow@hotmail.com>
2022-03-30 12:05:54 -0300

    preseed

--- src/utils/fakeForFloors.js
+++ src/utils/fakeForFloors.js
@@ -1,287 +1,131 @@
 export const buildings = [
   {
-    _id: "1",
-    name: "Mar del plata",
+    name: "Poseidon",
+    city: "Mar del plata",
     address:
       "Av. Colón 1114, B7600FXR Mar del Plata, Provincia de Buenos Aires",
+    imgBuilding:
+      "https://1.bp.blogspot.com/-TIkCvBHG_Eo/Xfpu6wPo_oI/AAAAAAAAToo/dqs_OmbToA8_QZZb4eTiqb-8G1VFX0tDgCLcBGAsYHQ/s400/Logo.jpg",
     Floors: [
       {
-        _id: "1",
         name: "1ª Floor",
+        imgFloor:
+          "https://archello.s3.eu-central-1.amazonaws.com/images/2018/04/05/-PAB4469.1522942283.9409.jpg",
         Desks: [
           {
-            _id: "1",
-            x: 50, //debe ser un FLOAT
-            y: 50, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
-            calendar: [
-              {
-                start: "28-03-2022T10:00",
-                end: "28-03-2022T15:30",
-                userId: "1",
-              },
-              {
-                start: "30-03-2022T10:00",
-                end: "30-03-2022T15:30",
-                userId: "1",
-              },
-              {
-                start: "28-03-2022T10:00",
-                end: "28-03-2022T15:30",
-                userId: "2",
-              },
-              {
-                start: "30-03-2022T10:00",
-                end: "30-03-2022T15:30",
-                userId: "2",
-              },
-            ],
+            positionX: 50,
+            positionY: 50,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "2",
-            x: 50, //debe ser un FLOAT
-            y: 90, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
-            calendar: [
-              {
-                start: "28-03-2022T10:00",
-                end: "28-03-2022T15:30",
-                userId: "1",
-              },
-              {
-                start: "30-03-2022T10:00",
-                end: "30-03-2022T15:30",
-                userId: "1",
-              },
-              {
-                start: "28-03-2022T10:00",
-                end: "28-03-2022T15:30",
-                userId: "2",
-              },
-              {
-                start: "30-03-2022T10:00",
-                end: "30-03-2022T15:30",
-                userId: "2",
-              },
-            ],
+            positionX: 50,
+            positionY: 90,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "3",
-            x: 50, //debe ser un FLOAT
-            y: 130, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 50,
+            positionY: 130,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "4",
-            x: 50, //debe ser un FLOAT
-            y: 170, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 50,
+            positionY: 170,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "5",
-            x: 75, //debe ser un FLOAT
-            y: 50, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 75,
+            positionY: 50,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "6",
-            x: 75, //debe ser un FLOAT
-            y: 90, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 75,
+            positionY: 90,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "7",
-            x: 75, //debe ser un FLOAT
-            y: 130, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 75,
+            positionY: 130,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "8",
-            x: 75, //debe ser un FLOAT
-            y: 170, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 75,
+            positionY: 170,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "9",
-            x: 150, //debe ser un FLOAT
-            y: 50, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 150,
+            positionY: 50,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "10",
-            x: 150, //debe ser un FLOAT
-            y: 90, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 150,
+            positionY: 90,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "11",
-            x: 150, //debe ser un FLOAT
-            y: 130, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 150,
+            positionY: 130,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "12",
-            x: 150, //debe ser un FLOAT
-            y: 170, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 150,
+            positionY: 170,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "13",
-            x: 175, //debe ser un FLOAT
-            y: 50, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 175,
+            positionY: 50,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "14",
-            x: 175, //debe ser un FLOAT
-            y: 90, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 175,
+            positionY: 90,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "15",
-            x: 175, //debe ser un FLOAT
-            y: 130, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
+            positionX: 175,
+            positionY: 130,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
           {
-            _id: "16",
-            x: 175, //debe ser un FLOAT
-            y: 170, //debe ser un FLOAT
-            rotation: 90, //debe ser un FLOAT
-          },
-        ],
-      },
-      {
-        _id: "2",
-        name: "2ª Floor",
-        Desks: [
-          {
-            _id: "11",
-            x: 50.05, //debe ser un FLOAT
-            y: 50.05, //debe ser un FLOAT
-            rotation: 0, //debe ser un FLOAT
-          },
-          {
-            _id: "12",
-            x: 10.155, //debe ser un FLOAT
-            y: 10.5115, //debe ser un FLOAT
-            rotation: 150.8007, //debe ser un FLOAT
-          },
-        ],
-      },
-      {
-        _id: "3",
-        name: "3ª Floor",
-        Desks: [
-          {
-            _id: "13",
-            x: 50.15, //debe ser un FLOAT
-            y: 50.05, //debe ser un FLOAT
-            rotation: 0, //debe ser un FLOAT
-          },
-          {
-            _id: "14",
-            x: 10.155, //debe ser un FLOAT
-            y: 10.5115, //debe ser un FLOAT
-            rotation: 150.8007, //debe ser un FLOAT
+            positionX: 175,
+            positionY: 170,
+            imgDesk: "",
+            rotation: 90,
+            calendar: [],
           },
         ],
       },
     ],
-  },
-  {
-    _id: "",
-    name: "Land Plaza",
-    address: "C. 6 572, B1902CLX La Plata, Provincia de Buenos Aires",
-    Floors: [
-      {
-        _id: "4",
-        name: "1ª Floor",
-        Desks: [
-          {
-            _id: "15",
-            x: 50.05, //debe ser un FLOAT
-            y: 50.05, //debe ser un FLOAT
-            rotation: 0, //debe ser un FLOAT
-            calendar: [
-              {
-                start: "28-03-2022T10:00",
-                end: "28-03-2022T15:30",
-                userId: "1",
-                deskId: "1",
-              },
-              {
-                start: "30-03-2022T10:00",
-                end: "30-03-2022T15:30",
-                userId: "1",
-                deskId: "3",
-              },
-              {
-                start: "28-03-2022T10:00",
-                end: "28-03-2022T15:30",
-                userId: "2",
-                deskId: "2",
-              },
-              {
-                start: "30-03-2022T10:00",
-                end: "30-03-2022T15:30",
-                userId: "2",
-                deskId: "1",
-              },
-            ],
-          },
-          {
-            x: 10.155, //debe ser un FLOAT
-            y: 10.5115, //debe ser un FLOAT
-            rotation: 150.8007, //debe ser un FLOAT
-          },
-        ],
-      },
-      {
-        _id: "5",
-        name: "2ª Floor",
-        Desks: [
-          {
-            _id: "16",
-            x: 50.05, //debe ser un FLOAT
-            y: 50.05, //debe ser un FLOAT
-            rotation: 0, //debe ser un FLOAT
-          },
-          {
-            _id: "17",
-            x: 10.155, //debe ser un FLOAT
-            y: 10.5115, //debe ser un FLOAT
-            rotation: 150.8007, //debe ser un FLOAT
-          },
-        ],
-      },
-    ],
-  },
-];
-
-export const calendar = [
-  {
-    start: "28-03-2022T10:00",
-    end: "28-03-2022T15:30",
-    userId: "1",
-    deskId: "1",
-  },
-  {
-    start: "30-03-2022T10:00",
-    end: "30-03-2022T15:30",
-    userId: "1",
-    deskId: "3",
-  },
-  {
-    start: "28-03-2022T10:00",
-    end: "28-03-2022T15:30",
-    userId: "2",
-    deskId: "2",
-  },
-  {
-    start: "30-03-2022T10:00",
-    end: "30-03-2022T15:30",
-    userId: "2",
-    deskId: "1",
   },
 ];
