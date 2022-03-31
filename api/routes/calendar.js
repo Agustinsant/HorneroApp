74ab5ab9cf9518d88cfca46793e187f166acb2c0
Joacohazee <joacohaze@gmail.com>
2022-03-31 14:22:06 -0300

    add route eventByUserId

--- api/routes/calendar.js
+++ api/routes/calendar.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const calendarRouter = express.Router()
-const { AddEventCalendar, getEventCalendarById, getAllEventCalendar, updateEventCalendarById, deleteEventCalendarById } = require('../controllers/calendarControllers')
+const { AddEventCalendar, getEventCalendarById, getAllEventCalendar, updateEventCalendarById, deleteEventCalendarById, getAllEventCalendarByUserId } = require('../controllers/calendarControllers')
 
 calendarRouter.post('/add/:idDesk', AddEventCalendar) // registra un evento del calendario
 
 calendarRouter.get('/:id', getEventCalendarById) // trae evento por id
 
 calendarRouter.get('/', getAllEventCalendar) // trae todos los eventos
+
+calendarRouter.get('/all/:userId', getAllEventCalendarByUserId) // trae todos los eventos por userId
 
 calendarRouter.put('/update/:id', updateEventCalendarById) // edita un evento
 
