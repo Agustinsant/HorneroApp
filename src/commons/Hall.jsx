7c51372a2e00819d1a7c8e2dca4f506fab669744
Ivan Winslow <ivan_winslow@hotmail.com>
2022-04-11 18:21:43 -0300

    canvasHallBug

--- src/commons/Hall.jsx
+++ src/commons/Hall.jsx
@@ -7,33 +7,60 @@ function Hall({ hall, userImg }) {
   if (hallColor === "#39B54A") hallColor = "#F7931E";
   return (
     <>
-      <RegularPolygon
-        id={hall._id}
-        x={hall.positionX}
-        y={hall.positionY}
-        sides={3}
-        radius={25}
-        fill={hallColor}
-        shadowBlur={4}
-        shadowOpacity={0.6}
-        onMouseEnter={(e) => {
-          const container = e.target.getStage().container();
-          container.style.cursor = "pointer";
-          e.target.fill("#258EA6");
-        }}
-        onMouseLeave={(e) => {
-          const container = e.target.getStage().container();
-          container.style.cursor = "default";
-          e.target.fill(hallColor);
-        }}
-      />
-      <Image
-        image={image}
-        width={15}
-        height={15}
-        x={hall.positionX - 7}
-        y={hall.positionY - 8}
-      />
+      {hall.userWillBeHere ? (
+        <>
+          <RegularPolygon
+            id={hall._id}
+            x={hall.positionX}
+            y={hall.positionY}
+            sides={3}
+            radius={25}
+            fill={hallColor}
+            shadowBlur={4}
+            shadowOpacity={0.6}
+            onMouseEnter={(e) => {
+              const container = e.target.getStage().container();
+              container.style.cursor = "pointer";
+              e.target.fill("#258EA6");
+            }}
+            onMouseLeave={(e) => {
+              const container = e.target.getStage().container();
+              container.style.cursor = "default";
+              e.target.fill(hallColor);
+            }}
+          />
+          <Image
+            image={image}
+            width={15}
+            height={15}
+            x={hall.positionX - 7}
+            y={hall.positionY - 8}
+          />
+        </>
+      ) : (
+        <>
+          <RegularPolygon
+            id={hall._id}
+            x={hall.positionX}
+            y={hall.positionY}
+            sides={3}
+            radius={25}
+            fill={hallColor}
+            shadowBlur={4}
+            shadowOpacity={0.6}
+            onMouseEnter={(e) => {
+              const container = e.target.getStage().container();
+              container.style.cursor = "pointer";
+              e.target.fill("#258EA6");
+            }}
+            onMouseLeave={(e) => {
+              const container = e.target.getStage().container();
+              container.style.cursor = "default";
+              e.target.fill(hallColor);
+            }}
+          />
+        </>
+      )}
     </>
   );
 }
