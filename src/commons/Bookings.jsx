970cd5da7ace682d8cb5271fb061298cfa01aa9b
Ivan Winslow <ivan_winslow@hotmail.com>
2022-04-12 13:38:19 -0300

    escritorioLoco

--- src/commons/Bookings.jsx
+++ src/commons/Bookings.jsx
@@ -1,66 +1,75 @@
 import { useParams } from "react-router";
 import { getUserBooking } from "../services/calendarServices";
-import { getUserById } from "../services/userServices"; 
+import { getUserById } from "../services/userServices";
 import { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
-import listPlugin from '@fullcalendar/list';
+import listPlugin from "@fullcalendar/list";
+import useFetch from "../hooks/useFetch";
 import { render } from "@testing-library/react";
 
+import "./booking.css";
+
 const Booking = () => {
-    const {userId} = useParams();
-    const [user, setUser]= useState({});
-    const [events, setEvents] = useState([]);
-    
+  const { userId } = useParams();
+  const [user, setUser] = useState({});
+  const [events, setEvents] = useState({});
 
-    useEffect(async () => {
-        if(userId) {
-        const userBooking = await getUserBooking(userId);
-        const user = await getUserById(userId)
-        setEvents(userBooking)
-        setUser(user)
-        }
-    },[userId])
+  useEffect(async () => {
+    const user = await getUserById(userId);
+    setUser(user);
+  }, [userId]);
 
+  const { data, loading } = useFetch(
+    `http://localhost:3001/api/calendar/all/${userId}`
+  );
 
-const renderEventContent = (eventInfo) =>{
-     const {extendedProps} = eventInfo.event.toPlainObject()
-    return(
-        <div className="event-booking">
-            <div className="building-book">{extendedProps.city}</div>
-            <div className="floor-book">{extendedProps.buildingName}</div>
-            <div className="floor-book">{extendedProps.floorName}</div>
-        </div>
-    )
-}
+  const renderEventContent = (eventInfo) => {
+    const { extendedProps } = eventInfo.event.toPlainObject();
+    return (
+      <div className="event-booking">
+        <div className="building-book">{extendedProps.city}</div>
+        <div className="floor-book">{extendedProps.buildingName}</div>
+        <div className="floor-book">{extendedProps.floorName}</div>
+      </div>
+    );
+  };
 
-    return(
-        <>
+  return (
+    <>
+      {!loading ? (
         <div className="profile_container">
-            <h6>Mi Reservas</h6>
-            <div className="profilePhotoContainer">
-                <div className="profile_photo">
-                    <img src={user.img} />
-                </div>
+          <h6>Mi Reservas</h6>
+          <div className="profilePhotoContainer">
+            <div className="profile_photo">
+              <img src={user.img} />
             </div>
-            <h5>{user.name}</h5>
-            <hr/>
-            <div className="booking_container">
-               
-                <FullCalendar
-                    locale={"es"}
-                    height={340}
-                    plugins={[listPlugin]}
-                    headerToolbar={{right: "next", center: "title", left: "prev" }}
-                    initialView="listMonth"
-                    events={events}
-                    eventContent={renderEventContent}
-                />
-            </div>
+          </div>
+          <h5>{user.name}</h5>
+          <hr />
+          <div className="booking_container">
+            <FullCalendar
+              locale={"es"}
+              height={340}
+              plugins={[listPlugin]}
+              headerToolbar={{ right: "next", center: "title", left: "prev" }}
+              initialView="listMonth"
+              events={data}
+              eventContent={renderEventContent}
+            />
+          </div>
         </div>
-        </>
-    )
-
-}
-
+      ) : (
+        <div className="booking_container">
+          <div className="deskCanFly">
+            <img
+              src="https://cdn.iconscout.com/icon/premium/png-256-thumb/employee-desk-24-1125109.png"
+              alt="loading"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Booking;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-function useFetch() {
+function useFetch(url) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
@@ -8,10 +9,11 @@ function useFetch() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-
-        setData(data);
+        const response = await axios({
+          method: "GET",
+          url: url,
+        });
+        setData(response.data);
         setLoading(false);
       } catch (error) {
         setError(error);
