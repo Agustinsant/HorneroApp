3453bef00c44edd08eac463864e2871444d4a21e
Ivan Winslow <ivan_winslow@hotmail.com>
2022-04-13 15:02:22 -0300

    referencias

---
+++ src/commons/CanvasReferences.jsx
@@ -0,0 +1,62 @@
+import { FaRegTimesCircle } from "react-icons/fa";
+
+function CanvasReferences({ setReferences }) {
+  const colors = {
+    desk: {
+      empty: "#39B54A",
+      concurred: "#BFD732",
+      full: "#444444 ",
+    },
+    hall: {
+      empty: "#F7931E",
+      concurred: "#BFD732",
+      full: "#444444 ",
+    },
+  };
+  return (
+    <div className="references">
+      <div className="references__container">
+        <div className="close-buton-container">
+          <FaRegTimesCircle onClick={() => setReferences(false)} />
+        </div>
+        <table className="references__table">
+          <tr>
+            <th>Elemento</th>
+            <th>Estado</th>
+            <th>Color</th>
+          </tr>
+          <tr>
+            <td>Escritorios</td>
+            <td>Disponibles</td>
+            <td>
+              <div className="emptyDesk"></div>
+            </td>
+          </tr>
+          <tr>
+            <td>Salas</td>
+            <td>Disponibles</td>
+            <td>
+              <div className="emptyHall"></div>
+            </td>
+          </tr>
+          <tr>
+            <td>Escritorios/Salas</td>
+            <td>Concurridos</td>
+            <td>
+              <div className="concurred"></div>
+            </td>
+          </tr>
+          <tr>
+            <td>Escritorios/Salas</td>
+            <td>Ocupados</td>
+            <td>
+              <div className="full"></div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default CanvasReferences;
